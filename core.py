c8ed053ef6041d3b9ce6d683c3394d14da6193ae
Robin Schroer <sulamiification@gmail.com>
2014-08-02 20:30:29 +0200

    core: add proper imports

--- core.py
+++ core.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+from player import Player
+from levels import Level, change_level
+from items import Item
+
 def play():
     """Start a basic new game"""
     P = Player()
